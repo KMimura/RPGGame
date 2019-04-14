d29c44ab99f1efaabcd3b19f2544fed8b6702661
KMimura <mimurakohei256@gmail.com>
2019-04-14 13:31:17 +0900

    travis test

--- success.sh
+++ success.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-curl -X POST -H "Content-Type: application/json" -d '{"status":0,"id":"'"$TRAVIS_COMMIT"'","message":"'"$TRAVIS_COMMIT_MESSAGE"'"}' $API_GATEWAY
+curl -X POST -H "Content-Type: application/json" -d '{"status":0,"url":"'"$TRAVIS_JOB_WEB_URL"'","message":"'"$TRAVIS_COMMIT_MESSAGE"'"}' $API_GATEWAY
