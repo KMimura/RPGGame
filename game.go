a19c54d04bfead3ecca366977518cf82211cd8e7
KMimura <40707315+KMimura@users.noreply.github.com>
2019-04-28 01:11:39 +0900

    changed Tilesheets

--- game.go
+++ game.go
@@ -4,11 +4,10 @@ import (
 	"bytes"
 	"image/color"
 
-	"github.com/KMimura/RPGGame/systems"
-
 	"github.com/EngoEngine/ecs"
 	"github.com/EngoEngine/engo"
 	"github.com/EngoEngine/engo/common"
+	"github.com/KMimura/RPGGame/systems"
 	"golang.org/x/image/font/gofont/gosmallcaps"
 )
 
@@ -17,7 +16,7 @@ type myScene struct{}
 func (*myScene) Type() string { return "myGame" }
 
 func (*myScene) Preload() {
-	engo.Files.Load("pics/greenoctocat.png", "pics/ghost.png", "tilemap/tilesheet_grass.png", "tilemap/tilesheet_snow.png")
+	engo.Files.Load("pics/greenoctocat.png", "pics/overworld_tileset_grass.png")
 	engo.Files.LoadReaderData("go.ttf", bytes.NewReader(gosmallcaps.TTF))
 	common.SetBackground(color.RGBA{255, 250, 220, 0})
 }
@@ -25,12 +24,13 @@ func (*myScene) Preload() {
 func (*myScene) Setup(u engo.Updater) {
 	engo.Input.RegisterButton("MoveRight", engo.KeyD, engo.KeyArrowRight)
 	engo.Input.RegisterButton("MoveLeft", engo.KeyA, engo.KeyArrowLeft)
-	engo.Input.RegisterButton("Jump", engo.KeySpace)
+	engo.Input.RegisterButton("MoveUp", engo.KeyW, engo.KeyArrowUp)
+	engo.Input.RegisterButton("MoveDown", engo.KeyS, engo.KeyArrowDown)
 	world, _ := u.(*ecs.World)
 	world.AddSystem(&common.RenderSystem{})
 	world.AddSystem(&systems.TileSystem{})
-	world.AddSystem(&systems.PlayerSystem{})
-	world.AddSystem(&systems.EnemySystem{})
+	// world.AddSystem(&systems.PlayerSystem{})
+	// world.AddSystem(&systems.EnemySystem{})
 }
 
 func (*myScene) Exit() {
