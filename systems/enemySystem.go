ed0137acb3f1e8e416778b6f3af315b98c33e5ac
KMimura <40707315+KMimura@users.noreply.github.com>
2019-05-05 16:57:12 +0900

    made a tool

--- systems/enemySystem.go
+++ systems/enemySystem.go
@@ -1,7 +1,12 @@
 package systems
 
 import (
+	"fmt"
+	"math/rand"
+	"time"
+
 	"github.com/EngoEngine/ecs"
+	"github.com/EngoEngine/engo"
 	"github.com/EngoEngine/engo/common"
 )
 
@@ -13,10 +18,6 @@ type Enemy struct {
 	movingState int
 	// 移動の残り時間
 	movingDuration int
-	// 移動の速度(0 ~ 2, 数値が高いほど早い)
-	velocity int
-	// 画面から消えているか
-	ifDissappeared bool
 }
 
 type EnemySystem struct {
@@ -35,97 +36,108 @@ func (es *EnemySystem) Remove(entity ecs.BasicEntity) {
 }
 
 func (es *EnemySystem) Update(dt float32) {
-	// rand.Seed(time.Now().UnixNano())
-	// // カメラとプレーヤーの位置を取得
-	// camX := camEntity.X()
-	// camY := camEntity.Y()
-	// playerX := playerInstance.SpaceComponent.Position.X
-	// playerY := playerInstance.SpaceComponent.Position.Y
-	// for _, o := range es.enemyEntity {
-	// 	// 画面に描画されていないオブジェクトは移動処理をしない
-	// 	if o.SpaceComponent.Position.X > camX+300 && o.SpaceComponent.Position.X < camX-300 && o.SpaceComponent.Position.Y > camY+300 && o.SpaceComponent.Position.Y < camY-300 && !o.ifDissappeared {
-	// 		// プレーヤーとの当たり判定
-	// 		if o.SpaceComponent.Position.X == playerX {
-	// 			fmt.Println("damaged")
-	// 		}
-	// 		o.SpaceComponent.Position.X -= float32(o.velocity + 1)
-	// 		// ジャンプをしていない場合
-	// 		if o.movingState == 0 {
-	// 			tmpNum := rand.Intn(100) - 95
-	// 			if tmpNum > 0 {
-
-	// 			}
-	// 			jumpTemp := rand.Intn(3)
-	// 			switch jumpTemp {
-	// 			case 0:
-	// 				o.jumpDuration = 15
-	// 			case 1:
-	// 				o.jumpDuration = 25
-	// 			case 2:
-	// 				o.jumpDuration = 35
-	// 			}
-	// 		}
-	// 		// ジャンプ処理
-	// 		if o.jumpState == 1 {
-	// 			// ジャンプをし終わっていない場合
-	// 			if o.jumpDuration > 0 {
-	// 				o.SpaceComponent.Position.Y -= 3
-	// 				o.jumpDuration -= 1
-	// 			} else {
-	// 				// ジャンプをし終わった場合
-	// 				o.jumpState = 2
-	// 			}
-	// 		} else {
-	// 			// 降下をし終わっていない場合
-	// 			if o.SpaceComponent.Position.Y < 212 {
-	// 				o.SpaceComponent.Position.Y += 3
-	// 			} else {
-	// 				// 降下し終わった場合
-	// 				o.jumpState = 0
-	// 			}
-	// 		}
-	// 	} else if o.ifDissappearing {
-	// 		o.SpaceComponent.Position.Y += 3
-	// 	}
-	// }
+	rand.Seed(time.Now().UnixNano())
+	// カメラとプレーヤーの位置を取得
+	camX := camEntity.X()
+	camY := camEntity.Y()
+	playerX := playerInstance.SpaceComponent.Position.X
+	playerY := playerInstance.SpaceComponent.Position.Y
+	for _, o := range es.enemyEntity {
+		// 画面に描画されていないオブジェクトは移動処理をしない
+		if o.SpaceComponent.Position.X < camX+300 && o.SpaceComponent.Position.X > camX-300 && o.SpaceComponent.Position.Y < camY+300 && o.SpaceComponent.Position.Y > camY-300 {
+			// プレーヤーとの当たり判定
+			if o.SpaceComponent.Position.X == playerX && o.SpaceComponent.Position.Y == playerY {
+				fmt.Println("damaged")
+			}
+			// 移動をしていない場合
+			if o.movingState == 0 {
+				tmpNum := rand.Intn(200) - 195
+				if tmpNum > 0 {
+					o.movingState = tmpNum
+					jumpTemp := rand.Intn(3)
+					switch jumpTemp {
+					case 0:
+						o.movingDuration = 10
+					case 1:
+						o.movingDuration = 15
+					case 2:
+						o.movingDuration = 20
+					}
+				}
+			}
+			if o.movingState == 1 {
+				// 上への移動処理
+				if o.movingDuration > 0 {
+					o.SpaceComponent.Position.X -= 3
+					o.movingDuration -= 1
+				} else {
+					o.movingState = 0
+				}
+			} else if o.movingState == 2 {
+				//右への移動
+				if o.movingDuration > 0 {
+					o.SpaceComponent.Position.Y += 3
+					o.movingDuration -= 1
+				} else {
+					o.movingState = 0
+				}
+			} else if o.movingState == 3 {
+				//下への移動
+				if o.movingDuration > 0 {
+					o.SpaceComponent.Position.X += 3
+					o.movingDuration -= 1
+				} else {
+					o.movingState = 0
+				}
+			} else if o.movingState == 4 {
+				//下への移動
+				if o.movingDuration > 0 {
+					o.SpaceComponent.Position.Y -= 3
+					o.movingDuration -= 1
+				} else {
+					o.movingState = 0
+				}
+			}
+		}
+	}
 }
 
 func (es *EnemySystem) New(w *ecs.World) {
-	// es.world = w
-	// Enemies := make([]*Enemy, 0)
-	// // ランダムで配置
-	// for i := 0; i < 44800; i++ {
-	// 	randomNum := rand.Intn(400)
-	// 	if randomNum == 0 {
-	// 		// 敵の作成
-	// 		enemy := Enemy{BasicEntity: ecs.NewBasic()}
-	// 		enemy.SpaceComponent = common.SpaceComponent{
-	// 			Position: engo.Point{X: float32(i), Y: float32(212)},
-	// 			Width:    30,
-	// 			Height:   30,
-	// 		}
-	// 		// 画像の読み込み
-	// 		texture, err := common.LoadedSprite("pics/ghost.png")
-	// 		if err != nil {
-	// 			fmt.Println("Unable to load texture: " + err.Error())
-	// 		}
-	// 		enemy.RenderComponent = common.RenderComponent{
-	// 			Drawable: texture,
-	// 			Scale:    engo.Point{X: 1.1, Y: 1.1},
-	// 		}
-	// 		enemy.RenderComponent.SetZIndex(1)
-	// 		es.texture = texture
-	// 		for _, system := range es.world.Systems() {
-	// 			switch sys := system.(type) {
-	// 			case *common.RenderSystem:
-	// 				sys.Add(&enemy.BasicEntity, &enemy.RenderComponent, &enemy.SpaceComponent)
-	// 			}
-	// 		}
-	// 		enemy.velocity = rand.Intn(3)
-	// 		Enemies = append(Enemies, &enemy)
-	// 	}
-	// 	es.enemyEntity = Enemies
-	// }
+	rand.Seed(time.Now().UnixNano())
+	es.world = w
+	Enemies := make([]*Enemy, 0)
+	// ランダムで配置
+	for i := 0; i < 4000; i++ {
+		randomNum := rand.Intn(400)
+		if randomNum == 0 {
+			// 敵の作成
+			enemy := Enemy{BasicEntity: ecs.NewBasic()}
+			enemy.SpaceComponent = common.SpaceComponent{
+				Position: engo.Point{X: float32(i), Y: float32(rand.Intn(300))},
+				Width:    30,
+				Height:   30,
+			}
+			// 画像の読み込み
+			texture, err := common.LoadedSprite("pics/ghost.png")
+			if err != nil {
+				fmt.Println("Unable to load texture: " + err.Error())
+			}
+			enemy.RenderComponent = common.RenderComponent{
+				Drawable: texture,
+				Scale:    engo.Point{X: 1.1, Y: 1.1},
+			}
+			enemy.RenderComponent.SetZIndex(1)
+			es.texture = texture
+			for _, system := range es.world.Systems() {
+				switch sys := system.(type) {
+				case *common.RenderSystem:
+					sys.Add(&enemy.BasicEntity, &enemy.RenderComponent, &enemy.SpaceComponent)
+				}
+			}
+			Enemies = append(Enemies, &enemy)
+		}
+		es.enemyEntity = Enemies
+	}
 	for _, system := range w.Systems() {
 		switch sys := system.(type) {
 		case *common.CameraSystem:
