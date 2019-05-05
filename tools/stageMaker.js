92415b5ffd3bd5fcabfd16a873a852e649e7ba75
KMimura <40707315+KMimura@users.noreply.github.com>
2019-05-05 20:35:01 +0900

    improved the tool

--- tools/stageMaker.js
+++ tools/stageMaker.js
@@ -4,11 +4,27 @@ const divideHeight = 16
 const app = new Vue({
     el : '#app',
     data : {
-        picPath : ""
+        picPath : "",
+        imgList : [],
+        contents:[],
     },
     methods : {
-        handleClick : function() {
-            this.divideImage(this.picPath)
+        clicked : function(){
+            $("#previewArea").empty();
+            console.log(this.contents)
+            for (let i = 0; i < this.contents.length; i++) {
+                let tr = $("<tr></tr>");
+                for (let j= 0; j < this.contents[i].length; j++) {
+                    let td = $("<td></td>");
+                    if (this.contents[i][j] != undefined ){
+                        td.css("background-image", "url(" + this.imgList[this.contents[i][j]] + ")")
+                    }else{
+                        td.css("display","none")
+                    }
+                    tr.append(td);
+                }
+                $("#previewArea").append(tr);
+            }
         },
         divideImage : function(imgSrc) {
             //画像の取得からロード後処理まで
@@ -16,7 +32,6 @@ const app = new Vue({
             img.origin = 'anonymous';
             tmp = this
             img.onload = function() {
-                alert(img)
                 tmp.segmentationImage(img, divideWidth, divideHeight);
             }
             img.src = imgSrc
@@ -26,8 +41,6 @@ const app = new Vue({
             // 画面には表示されない
             let canvas = $("<canvas width=" + divideWidth + " height=" + divideHeight + ">").get(0);
             let ctx = canvas.getContext("2d");
-            // 分割後のデータを保存する
-            let imgList = [];
             // 縦横の個数を取得する
             let wLength = img.width / divideWidth;
             let hLength = img.height / divideHeight;
@@ -35,13 +48,31 @@ const app = new Vue({
             for(let num = 0; num < wLength * hLength; num++) {
                 ctx.clearRect(0,0,canvas.width,canvas.divideHeight);
                 ctx.drawImage(img, divideWidth * (num % wLength), divideHeight* Math.floor(num / wLength), divideWidth, divideHeight, 0, 0, divideWidth, divideHeight);
-                imgList.push(canvas.toDataURL());
+                this.imgList.push(canvas.toDataURL());
             }
-            for(let num = 0; num < imgList.length; num++) {
-                let list = $("<li></li>"); 
-                list.css("background-image", "url(" + imgList[num] + ")"); 
-                $("ul").append(list);
-            };
+            console.log(this.imgList)
+            for (let y = 0; y < this.imgList.length / 15; y++){
+                let tr = $("<tr></tr>");
+                for (let x = y*15; x < y*15+14; x++){
+                    if (x >= this.imgList.length){
+                        break;
+                    }
+                    let td = $("<td></td>")
+                    td.css("background-image", "url(" + this.imgList[x] + ")")
+                    tr.append(td);
+                }
+                $("#sampleArea").append(tr);
+            }
         }    
+    },
+    created () {
+        for (let i = 0; i < 50; i++){
+            tmp = []
+            for (let j = 0; j < 50; j++){
+                tmp.push(undefined)
+            }
+            this.contents.push(tmp)
+        }
+        this.divideImage("overworld_tileset_grass.png")
     }
 })
